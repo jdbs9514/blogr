065635918baef9e0156f7b0fb43e0763a40516e5
Josue Benavides <jdbs9514@gmail.com>
2024-05-07 18:44:58 -0500

    start the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
+import hamburguer from "../images/icon-hamburger.svg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className="bg-orange-500">
+        <div className="flex justify-between mx-5 bg-orange-500">
+           <h2 className="text-3xl text-white font-bold my-12">Blogr</h2>
+           <img src={hamburguer} alt="hamburguer" className="my-14 cursor-pointer"/>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
