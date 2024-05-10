2822bafaa2fa141f0fa3549c6d8a7fe380dbeaf2
Josue Benavides <jdbs9514@gmail.com>
2024-05-10 18:03:03 -0500

    added dessign section

--- src/components/DesignSection.jsx
+++ src/components/DesignSection.jsx
@@ -1,8 +1,46 @@
+import editor from "../images/illustration-editor-mobile.svg";
 
 const DesignSection = () => {
-  return (
-    <div>DesignSection</div>
-  )
-}
+  const text = [
+    {
+      title: "Introducing an extensible editor",
+      description:
+        "Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. \
+      The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, \
+      videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or \
+      change the looks of a blog.",
+    },
 
-export default DesignSection
+    {
+      title: "Robust content management",
+      description:
+        "Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized \
+      categories, sections, format, or flow. With this functionality, you’re in full control.",
+    },
+  ];
+
+  return (
+    <section>
+      <div className="mt-[25%]">
+        <div className="text-center">
+          <h2 className="font-semibold text-xl text-purple-950">Designed for the future</h2>
+        </div>
+        <div className="mt-12">
+          <img src={editor} alt="editor" />
+        </div>
+        {text.map((item, index) => (
+          <div key={index}>
+            <div className="text-center mt-10">
+              <h2 className="font-semibold text-purple-950 text-2xl mx-14">{item.title}</h2>
+            </div>
+            <div className="mt-4 text-center">
+              <p className="px-9 text-zinc-500">{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default DesignSection;
