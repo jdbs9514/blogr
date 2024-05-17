314b7cff65554e5620e20647e0125124db5e0380
Josue Benavides <jdbs9514@gmail.com>
2024-05-16 20:08:47 -0500

    almost done

--- src/components/DesignSection.jsx
+++ src/components/DesignSection.jsx
@@ -39,17 +39,17 @@ const DesignSection = () => {
   ];
 
   return (
-    <section>
+    <section className="w-full">
       <div className="text-center">
         <h2 className="font-semibold text-2xl text-purple-950 lg:text-3xl mt-[25%] lg:mt-[10%]">
           Designed for the future
         </h2>
       </div>
       <div className="lg:mt-[5%] lg:flex lg:flex-row-reverse">
-        <div className="mt-12 flex justify-center lg:mt-[-10rem]">
-          <img src={imageSource} alt="editor" className="lg:w-[200rem] lg:ml-[500px]" />
+        <div className="mt-12 flex justify-center lg:mt-[-12rem] lg:scale-[1]">
+          <img src={imageSource} alt="editor" className="ml-[500]" />
         </div>
-        <div className="lg:ml-[100px]">
+        <div className="lg:w-[42%]">
           {text.map((item, index) => (
             <div key={index}>
               <div className="text-center mt-10 lg:text-start">
@@ -58,7 +58,7 @@ const DesignSection = () => {
                 </h2>
               </div>
               <div className="mt-4 text-center lg:text-start lg:mt-6">
-                <p className="px-9 text-zinc-500 lg:px-[55px] lg:text-xl">{item.description}</p>
+                <p className="px-9 text-zinc-500 lg:pl-[55px] lg:text-xl">{item.description}</p>
               </div>
             </div>
           ))}
